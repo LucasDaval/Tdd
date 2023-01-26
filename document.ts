d9a1c217d498bf8485b7db3fcd9132a5948ce364
Armand Dorard <armand.dorard@commoprices.com>
2023-01-26 16:08:24 +0100

    Add documents class

---
+++ document.ts
@@ -0,0 +1,8 @@
+export class Doc {
+    name: any;
+    signed: boolean;
+    constructor(name) {
+        this.name = name;
+        this.signed = false;
+    }
+}
--- functions/sign/index.ts
+++ functions/sign/index.ts
@@ -1,12 +1,4 @@
-export class Doc {
-    name: any;
-    signed: boolean;
-    constructor(name) {
-        this.name = name;
-        this.signed = false;
-    }
-}
-
+import {Doc} from "../../document";
 export function signed(document: Doc) {
     return document.signed
 }
