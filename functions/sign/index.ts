713a1912123089741c615f6f4ea927a7e103e99f
Armand Dorard <armand.dorard@commoprices.com>
2023-01-29 20:36:56 +0100

    restructuration of Sign : delete useless function

--- functions/sign/index.ts
+++
@@ -1,9 +0,0 @@
-import {File} from "../../class/File";
-
-export const signed = (document: File) => {
-    return document.signed
-}
-export const sign =(document: File) => {
-    if(document.signed === false) return document.signed = true
-    else throw new Error('Error, this document is already sign');
-}
